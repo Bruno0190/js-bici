84d9edc42675924e290d78790a4605748b2d60f0
Bruno0190 <bruno.varadero90@gmail.com>
2025-03-06 19:41:00 +0100

    feat: voglio ragionarci

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const bici = [
+    {
+        nome : "bianchi",
+        peso : 10,
+    },
+
+    {
+        nome : "mountain-bike",
+        peso : 26,
+    },
+    
+    {
+        nome : "canyon",
+        peso : 14,
+    },
+
+    {
+        nome : "granville",
+        peso : 8,
+    },
+]
+
+bici.forEach(function(curBici, index) {
+    
+
+});
